01ae475460af274458e8afc4fa6906b57a26dedc
pulok <khandokarmeherab@gmail.com>
2022-10-08 15:03:35 +0600

    main added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import Main from './layout/Main';
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>
+    }
+  ])
+  return (
+    <div>
+     <RouterProvider router ={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
