41828b43523328939cdc1eba88a459ab454478d5
pulok <khandokarmeherab@gmail.com>
2022-10-08 16:25:09 +0600

    link,and route added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Main from './layout/Main';
-
+import Shop from './components/Shop/Shop';
+import Order from './components/Orders/Order';
+import Inventory from './components/Inventory';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Main></Main>
+      element: <Main></Main>,
+      children:[
+        {
+          path:'/',
+          element:<Shop></Shop>
+        },
+        {
+          path: '/orders',
+          element: <Order></Order>
+        },
+        {
+          path: '/inventory',
+          element:<Inventory></Inventory>
+        },
+        {
+          path: '/about',
+          element:<About></About>
+        }
+        
+
+
+      ]
+    },
+    {
+      path: '/about',
+      element:<About></About>
     }
   ])
   return (
