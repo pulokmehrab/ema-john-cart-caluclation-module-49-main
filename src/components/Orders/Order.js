1afb9c99f73041e411f2f74379b39cf448b8e550
pulok <khandokarmeherab@gmail.com>
2022-10-09 17:13:44 +0600

    cart added in order section

--- src/components/Orders/Order.js
+++ src/components/Orders/Order.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
 
 const Order = () => {
     const {Products,  initialCart}=useLoaderData();
+    const [cart,setcart]=useState(initialCart);
     return (
         <div>
     <h1>this is ordder section: {Products.length}</h1>
-    <h2>previous cart: {initialCart.length}</h2>
+    <h2>previous cart: {cart.length}</h2>
+    <Cart cart={cart}></Cart>
         </div>
     );
 };
